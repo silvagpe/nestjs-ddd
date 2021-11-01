16ef1cce60613addf544ae7ea4e986e408a29281
Rafael Da Silva <silvagpe@gmail.com>
2021-11-01 08:13:32 -0300

    testes typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,13 @@ import { Module } from '@nestjs/common';
 import { InfrastructureModule } from './infrastructure/infrastructure.module';
 import { ApiModule } from './api/api.module';
 import { DomainModule } from './domain/domain.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [InfrastructureModule, ApiModule, DomainModule],  
+  imports: [
+    ApiModule,
+    DomainModule,
+    InfrastructureModule,    
+    TypeOrmModule.forRoot()
+    ],  
 })
 export class AppModule {}
